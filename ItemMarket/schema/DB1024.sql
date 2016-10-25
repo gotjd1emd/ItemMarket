592c29ce26362d85bd8bf41666321787cd43fac3
essenonvideri <essenonvideri@naver.com>
2016-10-25 20:30:44 +0900

    update sql

--- ItemMarket/schema/DB1024.sql
+++ ItemMarket/schema/DB1024.sql
@@ -9,28 +9,30 @@ create table userinfo (
  
 );
 
+select * from userinfo;
+drop table userinfo;
 
 --기본 데이터
-insert into userinfo (id,tel,email,location,cash) values ('aaa','01093441366','awaynee@hanmail.net','서울시 금천구',190000);
-insert into userinfo (id,tel,email,location,cash) values ('bbb','01011112222','bbbb@naver.com','서울시 구로구',90000);
-insert into userinfo (id,tel,email,location,cash) values ('ccc','01033334444','cccc@gmail.com','서울시 동작구',290000);
-insert into userinfo (id,tel,email,location,cash) values ('ddd','01055556666','dddd@hanmail.net','서울시 강남구',390000);
-insert into userinfo (id,tel,email,location,cash) values ('eee','01077778888','eeee@naver.com','서울시 강동구',490000);
-insert into userinfo (id,tel,email,location,cash) values ('fff','01088889999','ffff@gmail.com','서울시 누원구',590000);
+insert into userinfo (id,password,tel,email,location,cash) values ('aaa','1234','01093441366','awaynee@hanmail.net','서울시 금천구',190000);
+insert into userinfo (id,password,tel,email,location,cash) values ('bbb','1234','01011112222','bbbb@naver.com','서울시 구로구',90000);
+insert into userinfo (id,password,tel,email,location,cash) values ('ccc','1234','01033334444','cccc@gmail.com','서울시 동작구',290000);
+insert into userinfo (id,password,tel,email,location,cash) values ('ddd','1234','01055556666','dddd@hanmail.net','서울시 강남구',390000);
+insert into userinfo (id,password,tel,email,location,cash) values ('eee','1234','01077778888','eeee@naver.com','서울시 강동구',490000);
+insert into userinfo (id,password,tel,email,location,cash) values ('fff','1234','01088889999','ffff@gmail.com','서울시 누원구',590000);
 
-insert into userinfo (id,tel,email,location,cash) values ('ggg','01023234545','ggg@hanmail.net','서울시 서대문구',670000);
-insert into userinfo (id,tel,email,location,cash) values ('hhh','01012122323','hhh@naver.com','서울시 강서구',350000);
-insert into userinfo (id,tel,email,location,cash) values ('iii','01045456767','iii@gmail.com','서울시 중구',210000);
-insert into userinfo (id,tel,email,location,cash) values ('jjj','01078784343','jjj@hanmail.net','서울시 중랑구',750000);
-insert into userinfo (id,tel,email,location,cash) values ('kkk','01087879898','kkk@naver.com','서울시 서초구',640000);
-insert into userinfo (id,tel,email,location,cash) values ('lll','01013133535','lll@gmail.com','서울시 관악구',180000);
+insert into userinfo (id,password,tel,email,location,cash) values ('ggg','1234','01023234545','ggg@hanmail.net','서울시 서대문구',670000);
+insert into userinfo (id,password,tel,email,location,cash) values ('hhh','1234','01012122323','hhh@naver.com','서울시 강서구',350000);
+insert into userinfo (id,password,tel,email,location,cash) values ('iii','1234','01045456767','iii@gmail.com','서울시 중구',210000);
+insert into userinfo (id,password,tel,email,location,cash) values ('jjj','1234','01078784343','jjj@hanmail.net','서울시 중랑구',750000);
+insert into userinfo (id,password,tel,email,location,cash) values ('kkk','1234','01087879898','kkk@naver.com','서울시 서초구',640000);
+insert into userinfo (id,password,tel,email,location,cash) values ('lll','1234','01013133535','lll@gmail.com','서울시 관악구',180000);
 
 
 select * from  userinfo; 
 
   
 --시퀀스 준비
-grant create sequence to lee;   --(생성 계정 대입 )
+grant create sequence to root;   --(생성 계정 대입 )
 
 --시퀀스생성
 create sequence borderinfo_seq;  --(테이블_seq)
@@ -54,7 +56,7 @@ itemState varchar2(20)
 
 );
 
---drop table borderInfo;
+drop table borderInfo;
 
 --데이터 준비
 insert into borderInfo (id,border_number,content,itemName,money,dayDate,category,sub_Category,itemState) values ('aaa',borderInfo_seq.nextval,'▶▶급매◀◀','갤럭시S6',1000,sysdate,'디지털/가전제품','휴대폰','구매대기중');
@@ -63,10 +65,9 @@ insert into borderInfo (id,border_number,content,itemName,money,dayDate,category
 
 --insert into borderInfo (id,border_number,content,itemName,money,dayDate,category,sub_Category,itemState) values ('ddd',borderInfo_seq.nextval,'▶구매자찾습니다◀','skirt',1000,sysdate,'의류','여성의류','구매대기중');
 insert into borderInfo (id,border_number,content,itemName,money,dayDate,category,sub_Category,itemState) values ('eee',borderInfo_seq.nextval,'▶강동구 사시는분!◀','목도리',44000,sysdate,'악세서리','기타','구매대기중');
-insert into borderInfo (id,border_number,content,itemName,money,dayDate,category,sub_Category,itemState) values ('fff',borderInfo_seq.nextval,'▶직접 찾아갑니다.◀','선글라스',3300,sysdate,'악세서리','기타','구매대기중');
-
+--insert into borderInfo (id,border_number,content,itemName,money,dayDate,category,sub_Category,itemState) values ('fff',borderInfo_seq.nextval,'▶직접찾아갑니다.◀','선글라스',3300,sysdate,'악세서리','기타','구매대기중');
 --insert into borderInfo (id,border_number,content,itemName,money,dayDate,category,sub_Category,itemState) values ('ggg',borderInfo_seq.nextval,'▶▶빠른 연락◀◀','골프채',1000,sysdate,'스포츠','골프','구매대기중');
-insert into borderInfo (id,border_number,content,itemName,money,dayDate,category,sub_Category,itemState) values ('hhh',borderInfo_seq.nextval,'▶▶초저가 판매◀◀','외장하드',44000,sysdate,'디지털/가전제품','컴퓨터','구매대기중');
+--insert into borderInfo (id,border_number,content,itemName,money,dayDate,category,sub_Category,itemState) values ('hhh',borderInfo_seq.nextval,'▶▶초저가판매◀◀','외장하드',44000,sysdate,'디지털/가전제품','컴퓨터','구매대기중');
 insert into borderInfo (id,border_number,content,itemName,money,dayDate,category,sub_Category,itemState) values ('iii',borderInfo_seq.nextval,'▶택배가능합니다.◀','텀블러',3300,sysdate,'생활/가정','커피','구매대기중');
 
 
@@ -78,21 +79,22 @@ create table img_Border(
   img varchar2(20)  -- 이미지 
 );
 
+--drop table img_border;
 
 --데이터 준비     
 
 --borderifo 검색후 border_Number 맞춰주세요~          
 select * from borderinfo;
                                               --  ▼ 
-insert into img_Border(border_Number,img) values (21, '이미지1.jpg');
-insert into img_Border(border_Number,img) values (22, '이미지2.jpg');
-insert into img_Border(border_Number,img) values (23, '이미지3.jpg');
-insert into img_Border(border_Number,img) values (42, '이미지4.jpg');
-insert into img_Border(border_Number,img) values (45, '이미지5.jpg');
-insert into img_Border(border_Number,img) values (46, '이미지6.jpg');
+insert into img_Border(border_Number,img) values (1, '이미지1.jpg');
+insert into img_Border(border_Number,img) values (2, '이미지2.jpg');
+insert into img_Border(border_Number,img) values (3, '이미지3.jpg');
+insert into img_Border(border_Number,img) values (4, '이미지4.jpg');
+insert into img_Border(border_Number,img) values (7, '이미지5.jpg');
+
 
 select * from img_Border;
---drop  table img_Border;
+-- drop  table img_Border;
 --select * from img_Border;
 
 
@@ -112,7 +114,8 @@ create table  messenger(
 
 select * from messenger;
 
-
+select * from trade_history;
+drop table trade_history;
 
 --상호간 필요한 정보 
 create table trade_history (
@@ -123,32 +126,26 @@ create table trade_history (
   cash number(7),  -- 지불액
   border_number constraint trading_bordernumber_fk references borderinfo(border_number) primary key, --글 게시 번호
   daydate date not null, -- 날짜 
-  trade_state varcher2
+  trade_state varchar2(40)
   
 );
-
---  insert into trade_history (buyer,seller,itemName,cash,border_number,daydate) values ('fff', 'aaa','갤럭시S6',2000,1,'2016-10-24');
---  insert into trade_history (buyer,seller,itemName,cash,border_number,daydate) values ('ddd', 'bbb','T-100',3000,2,'2016-10-24');
---  insert into trade_history (buyer,seller,itemName,cash,border_number,daydate) values ('eee', 'ccc','CalvinKlein',4400,3,'2016-10-24');  
---  drop table trade_history;
-
-
-
-
---  border Info 검색후 border-number 숫자를 변경해 주세요                                               										  --  ▼
-select * from borderInfo;                                                                                 --  ▼
+--  border Info 검색후 border-number 숫자를 변경해 주세요                                               										  
+select * from borderInfo;    
+select * from trade_history;
+                                                                                                                           --  ▼   
+  insert into trade_history (buyer,seller,itemName,cash,border_number,daydate,trade_state) values ('fff', 'aaa','갤럭시S6',2000,1,'2016-10-24','거래완료');
+  insert into trade_history (buyer,seller,itemName,cash,border_number,daydate,trade_state) values ('ddd', 'bbb','T-100',3000,2,'2016-10-24','거래완료');
+  insert into trade_history (buyer,seller,itemName,cash,border_number,daydate,trade_state) values ('eee', 'ccc','CalvinKlein',4400,3,'2016-10-24','거래완료');                                                                               --  ▼
                                                                                                           --  ▼
-insert into  trade_history (buyer,seller,itemName,cash,border_number,daydate) values ('aaa','ddd','갤럭시S6',120000,21,sysdate);
-insert into  trade_history (buyer,seller,itemName,cash,border_number,daydate) values ('bbb','fff','T-100',200000,22,sysdate);
-insert into  trade_history (buyer,seller,itemName,cash,border_number,daydate) values ('ccc','ggg','CalvinKlein',30000,23,sysdate);
-insert into  trade_history (buyer,seller,itemName,cash,border_number,daydate) values ('eee','hhh','목도리',4000,42,sysdate);
+  insert into  trade_history (buyer,seller,itemName,cash,border_number,daydate,trade_state) values ('bbb','eee','목도리',120000,4,sysdate,'거래완료');
+  insert into  trade_history (buyer,seller,itemName,cash,border_number,daydate,trade_state) values ('ccc','iii','텀블러',200000,7,sysdate,'거래완료');
 
-
+--drop table borderinfo;
 
   select * from trade_history;
 
 
---cash_history table   id, 구매내역 
+--cash_history table   id, 구매내역 -receiver 구매자 
 create table cash_History (
   id varchar2(20) constraint cash_History_id_fk references userinfo  (id),  -- id 
   itemName varchar2(20) constraint cash_History_itemName_fk references borderInfo(itemName),  -- 물품 이름
@@ -158,10 +155,16 @@ create table cash_History (
   
 );
 
-insert into cash_history (id,itemName,mileage,saveDate) values ('aaa','CalvinKlein',100,'2016-10-25'); 
-insert into cash_history (id,itemName,mileage,saveDate) values ('bbb','갤럭시S6',500,'2016-10-25'); 
-insert into cash_history (id,itemName,mileage,saveDate) values ('ccc','T-100',900,'2016-10-25'); 
 
+
+  select * from trade_history;
+
+
+insert into cash_history (id,itemName,mileage,saveDate,current_cash) values ('fff','갤럭시S6',20,'2016-10-25',588000); 
+insert into cash_history (id,itemName,mileage,saveDate,current_cash) values ('ddd','T-100',30,'2016-10-25',287000); 
+insert into cash_history (id,itemName,mileage,saveDate,current_cash) values ('eee','CalvinKlein',44,'2016-10-25',485600); 
+insert into cash_history (id,itemName,mileage,saveDate,current_cash) values ('bbb','목도리',1200,'2016-10-25',170000);
+insert into cash_history (id,itemName,mileage,saveDate,current_cash) values ('ccc','텀블러',2000,'2016-10-25',190000); 
 
 select * from cash_history;
 

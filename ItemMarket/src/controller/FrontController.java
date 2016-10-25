4b93d617ccd12380fcbc4a5adf1083e13ca1331e
gotjd1emd <gotjd1emd@gmail.com>
2016-10-25 18:03:29 +0900

    add controller and util
    action interface
    FrontController
    action.properties
    actionMappingListener

---
+++ ItemMarket/src/controller/FrontController.java
@@ -0,0 +1,58 @@
+package controller;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FrontController
+ */
+@WebServlet("/front")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	Map<String, Action> map;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FrontController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		ServletContext application = config.getServletContext();
+		map = (Map<String, Action>)application.getAttribute("map");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String key = request.getParameter("command");
+		
+		Action action = map.get(key);
+		action.execute(request, response);
+	}
+
+}

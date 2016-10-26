6dd128d8598764573b2121be9685340edbb07feb
despotofkingdom <david158720@naver.com>
2016-10-26 09:39:42 +0900

    7. MemoBox - Servlce,Controller
    7. memobox service contorller

---
+++ ItemMarket/src/controller/MemoBox.java
@@ -0,0 +1,27 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.dto.MemoDTO;
+import model.service.ItemMarketService;
+
+public class MemoBox implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		
+		List<MemoDTO> memobox = ItemMarketService.memobox(id);
+		System.out.println("memobox : " + memobox);
+		request.setAttribute("memobox", memobox);
+		
+		
+		
+	}
+
+}
--- ItemMarket/src/model/service/ItemMarketService.java
+++ ItemMarket/src/model/service/ItemMarketService.java
@@ -1,10 +1,12 @@
 package model.service;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.dao.ItemMarketDAOImpl;
 import model.dto.BorderDTO;
+import model.dto.MemoDTO;
 
 public class ItemMarketService {
 	/**
@@ -22,6 +24,22 @@ public class ItemMarketService {
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
+		return list;
+	}
+	
+	/**
+	 * 7. 메신저함
+	 * 받은사람, 보내는사람, 내용
+	 */
+	public static List<MemoDTO> memobox(String id){
+		List<MemoDTO> list = new ArrayList<>();
+		ItemMarketDAOImpl dao = new ItemMarketDAOImpl();
+		try{
+			list = dao.memobox(id);
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		
 		return list;
 	}
 }

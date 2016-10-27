0807d9533e749c0f51fdbb57035704d54a36ccf5
soheel <dlthgml916@gmail.com>
2016-10-27 14:38:53 +0900

    ㅇㅇ
    ㅇㅇ

--- ItemMarket/src/controller/Write.java
+++ ItemMarket/src/controller/Write.java
@@ -16,27 +16,28 @@ public class Write implements Action {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//아이디,
-		String id = request.getParameter("id");
+		//String id = request.getParameter("id");
+		String id="aaa";
 		int bordernumber=30;
 		String content = request.getParameter("content");
 		String title = request.getParameter("title");
-		int price = Integer.parseInt(request.getParameter("price"));
+		String price = request.getParameter("price").trim();
 		String category = request.getParameter("category");
 		String subcategory = request.getParameter("subcategory");
 		String itemState = "구매대기중";
 		
-		BorderDTO borderDTO = new BorderDTO(id, bordernumber, content, title, price, "", category, subcategory, itemState);
+		//BorderDTO borderDTO = new BorderDTO(id, bordernumber, content, title, Integer.parseInt(price), "", category, subcategory, itemState);
 		
 		//ItemMarketService.imgWrite(borderDTO.getBorderNumber(), imgName);
 		PrintWriter out = response.getWriter();
-		if(ItemMarketService.write(borderDTO)>0){
+		/*if(ItemMarketService.write(borderDTO)>0){
 			response.sendRedirect("view/noticeboard.jsp");
 		}else{
 			out.println("<script>");
 			out.println("alert('글이 삽입되지 않았습니다.')");
 			out.println("history.back()");
 			out.println("</script>");
-		}
+		}*/
 		
 	}
 
